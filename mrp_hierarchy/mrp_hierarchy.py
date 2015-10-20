b2124249d0e29ff76a9d0fba09f04f215b401531
Vinod Singh <vinod.singh@verts.co.in>
2015-10-20 18:12:21 +0530

    MRP-hierarchy Module enhanced according to npg odoo version

--- mrp_hierarchy/mrp_hierarchy.py
+++ mrp_hierarchy/mrp_hierarchy.py
@@ -53,11 +53,11 @@ class StockMove(osv.osv):
 class mrp_bom(osv.osv):
     _inherit ='mrp.bom'
     
-    def _bom_explode(self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None):
+    def _bom_explode(self, cr, uid, bom, product, factor, properties=None, level=0, routing_id=False, previous_products=None, master_bom=None, context=None):
         """ Finds Products and Work Centers for related BoM for manufacturing order.
         @param bom: BoM of particular product template.
         @param product: Select a particular variant of the BoM. If False use BoM without variants.
-        @param factor: Factor of product UoM.
+        @param factor: Factor represents the quantity, but in UoM of the BoM, taking into account the numbers produced by the BoM
         @param properties: A List of properties Ids.
         @param level: Depth level to find BoM lines starts from 10.
         @param previous_products: List of product previously use by bom explore to avoid recursion
@@ -65,9 +65,10 @@ class mrp_bom(osv.osv):
         @return: result: List of dictionaries containing product details.
                  result2: List of dictionaries containing Work Center details.
         """
+        uom_obj = self.pool.get("product.uom")
         routing_obj = self.pool.get('mrp.routing')
-        all_prod = [] + (previous_products or [])
         master_bom = master_bom or bom
+
 
         def _factor(factor, product_efficiency, product_rounding):
             factor = factor / (product_efficiency or 1.0)
@@ -97,46 +98,50 @@ class mrp_bom(osv.osv):
                 })
 
         for bom_line_id in bom.bom_line_ids:
-            if bom_line_id.date_start and bom_line_id.date_start > time.strftime(DEFAULT_SERVER_DATETIME_FORMAT) or \
-                bom_line_id.date_stop and bom_line_id.date_stop > time.strftime(DEFAULT_SERVER_DATETIME_FORMAT):
+            if bom_line_id.date_start and bom_line_id.date_start > time.strftime(DEFAULT_SERVER_DATE_FORMAT) or \
+                bom_line_id.date_stop and bom_line_id.date_stop < time.strftime(DEFAULT_SERVER_DATE_FORMAT):
                     continue
-            # check properties
-            if set(map(int,bom_line_id.property_ids or [])) - set(properties or []):
-                continue
             # all bom_line_id variant values must be in the product
             if bom_line_id.attribute_value_ids:
                 if not product or (set(map(int,bom_line_id.attribute_value_ids or [])) - set(map(int,product.attribute_value_ids))):
                     continue
 
-            if bom_line_id.product_id.id in all_prod:
+            if set(map(int, bom_line_id.property_ids or [])) - set(properties or []):
+                continue
+
+            if previous_products and bom_line_id.product_id.product_tmpl_id.id in previous_products:
                 raise osv.except_osv(_('Invalid Action!'), _('BoM "%s" contains a BoM line with a product recursion: "%s".') % (master_bom.name,bom_line_id.product_id.name_get()[0][1]))
-            all_prod.append(bom_line_id.product_id.id)
-            
-            if bom_line_id.type != "phantom":
+
+            quantity = _factor(bom_line_id.product_qty * factor, bom_line_id.product_efficiency, bom_line_id.product_rounding)
+            bom_id = self._bom_find(cr, uid, product_id=bom_line_id.product_id.id, properties=properties, context=context)
+
+            #If BoM should not behave like PhantoM, just add the product, otherwise explode further
+            if bom_line_id.type != "phantom" and (not bom_id or self.browse(cr, uid, bom_id, context=context).type != "phantom"):
                 result.append({
                     'name': bom_line_id.product_id.name,
                     'product_id': bom_line_id.product_id.id,
-                    'product_qty': _factor(bom_line_id.product_qty * factor, bom_line_id.product_efficiency, bom_line_id.product_rounding),
+                    'product_qty': quantity,
                     'product_uom': bom_line_id.product_uom.id,
-                    'product_uos_qty': bom_line_id.product_uos and bom_line_id.product_uos_qty * factor or False,
+                    'product_uos_qty': bom_line_id.product_uos and _factor(bom_line_id.product_uos_qty * factor, bom_line_id.product_efficiency, bom_line_id.product_rounding) or False,
                     'product_uos': bom_line_id.product_uos and bom_line_id.product_uos.id or False,
                     #Verts Added sequence field
                     'bom_seq' : bom_line_id.sequence,
                 })
+            elif bom_id:
+                all_prod = [bom.product_tmpl_id.id] + (previous_products or [])
+                bom2 = self.browse(cr, uid, bom_id, context=context)
+                # We need to convert to units/UoM of chosen BoM
+                factor2 = uom_obj._compute_qty(cr, uid, bom_line_id.product_uom.id, quantity, bom2.product_uom.id)
+                quantity2 = factor2 / bom2.product_qty
+                res = self._bom_explode(cr, uid, bom2, bom_line_id.product_id, quantity2,
+                    properties=properties, level=level + 10, previous_products=all_prod, master_bom=master_bom, context=context)
+                result = result + res[0]
+                result2 = result2 + res[1]
             else:
-                bom_id = self._bom_find(cr, uid, bom_line_id.product_uom.id, product_id=bom_line_id.product_id.id, properties=properties)
-                if bom_id:
-                    bom2 = self.browse(cr, uid, bom_id)  
-                    res = self._bom_explode(cr, uid, bom2, bom_line_id.product_id, factor,
-                        properties=properties, level=level + 10, previous_products=all_prod, master_bom=master_bom)
-                    result = result + res[0]
-                    result2 = result2 + res[1]
-                else:
-                    raise osv.except_osv(_('Invalid Action!'), _('BoM "%s" contains a phantom BoM line but the product "%s" don\'t have any BoM defined.') % (master_bom.name,bom_line_id.product_id.name_get()[0][1]))
+                raise osv.except_osv(_('Invalid Action!'), _('BoM "%s" contains a phantom BoM line but the product "%s" does not have any BoM defined.') % (master_bom.name,bom_line_id.product_id.name_get()[0][1]))
 
         return result, result2
     
-
 class mrp_production(osv.osv):
     _inherit ='mrp.production'
     _order = 'name'
@@ -162,6 +167,7 @@ class mrp_production(osv.osv):
     
     def _make_consume_line_from_data(self, cr, uid, production, product, uom_id, qty, uos_id, uos_qty, bom_seq, context=None):
         stock_move = self.pool.get('stock.move')
+        loc_obj = self.pool.get('stock.location')
         # Internal shipment is created for Stockable and Consumer Products
         if product.type not in ('product', 'consu'):
             return False
@@ -186,16 +192,20 @@ class mrp_production(osv.osv):
             'location_dest_id': destination_location_id,
             'company_id': production.company_id.id,
             'procure_method': prev_move and 'make_to_stock' or self._get_raw_material_procure_method(cr, uid, product, context=context), #Make_to_stock avoids creating procurement
+            
             'raw_material_production_id': production.id,
             #this saves us a browse in create()
             'price_unit': product.standard_price,
             'origin': production.name,
+            'warehouse_id': loc_obj.get_warehouse(cr, uid, production.location_src_id, context=context),
+            'group_id': production.move_prod_id.group_id.id,
             'bom_seq':bom_seq,
         }, context=context)
         
         if prev_move:
             prev_move = self._create_previous_move(cr, uid, move_id, product, prod_location_id, source_location_id, context=context)
-        return move_id    
+            stock_move.action_confirm(cr, uid, [prev_move], context=context)
+        return move_id  
 
     def _make_production_consume_line(self, cr, uid, line, context=None):
         return self._make_consume_line_from_data(cr, uid, line.production_id, line.product_id, line.product_uom.id, line.product_qty, line.product_uos.id, line.product_uos_qty, line.bom_seq, context=context)
@@ -207,62 +217,69 @@ class procurement_order(osv.osv):
     _columns = {
                 'bom_seq':fields.integer("BoM line Sequence"),
                 }
-     
-    def make_mo(self, cr, uid, ids, context={}):
-        """ Make Manufacturing(production) order from procurement
-        @return: New created Production Orders procurement wise
-        """
-        res = {}
-        company = self.pool.get('res.users').browse(cr, uid, uid, context).company_id
-        production_obj = self.pool.get('mrp.production')
-        bom_obj = self.pool.get('mrp.bom')
-        procurement_obj = self.pool.get('procurement.order')
-        for procurement in procurement_obj.browse(cr, uid, ids, context=context):
-            if self.check_bom_exists(cr, uid, [procurement.id], context=context):
-                if procurement.bom_id:
-                    bom_id = procurement.bom_id.id
-                    routing_id = procurement.bom_id.routing_id.id
-                else:
-                    properties = [x.id for x in procurement.property_ids]
-                    bom_id = bom_obj._bom_find(cr, uid, procurement.product_uom.id,
-                        product_id=procurement.product_id.id, properties=properties)
-                    bom = bom_obj.browse(cr, uid, bom_id, context=context)
-                    routing_id = bom.routing_id.id
-
-                res_id = procurement.move_dest_id and procurement.move_dest_id.id or False
-                newdate = datetime.strptime(procurement.date_planned, '%Y-%m-%d %H:%M:%S') - relativedelta(days=procurement.product_id.produce_delay or 0.0)
-                newdate = newdate - relativedelta(days=company.manufacturing_lead)
-                #create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
-                sequence_number =  procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.name or ''
-                #if procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id :
-                produce_id = production_obj.create(cr, SUPERUSER_ID, {
-                    'origin': procurement.origin,
-                    'product_id': procurement.product_id.id,
-                    'product_qty': procurement.product_qty,
-                    'product_uom': procurement.product_uom.id,
-                    'product_uos_qty': procurement.product_uos and procurement.product_uos_qty or False,
-                    'product_uos': procurement.product_uos and procurement.product_uos.id or False,
-                    'location_src_id': procurement.location_id.id,
-                    'location_dest_id': procurement.location_id.id,
-                    'bom_id': bom_id,
-                    'routing_id': routing_id,
-                    'date_planned': newdate.strftime('%Y-%m-%d %H:%M:%S'),
-                    'move_prod_id': res_id,
-                    'company_id': procurement.company_id.id,
-                    'production_id':procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id or False,
+    
+    def _prepare_mo_vals(self, cr, uid, procurement, context=None):
+        res = super(procurement_order, self)._prepare_mo_vals(cr, uid, procurement, context=context)
+        sequence_number =  procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.name or ''
+        #if procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id :
+        res.update({'production_id':procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id or False,
                     'name':sequence_number + (procurement.bom_seq and ('-' + str(procurement.bom_seq)) or '')
                 })
-                
-                res[procurement.id] = produce_id
-                self.write(cr, uid, [procurement.id], {'production_id': produce_id})
-
-                procurement.refresh()
-                self.production_order_create_note(cr, uid, procurement, context=context)
-                production_obj.action_compute(cr, uid, [produce_id], properties=[x.id for x in procurement.property_ids])
-                production_obj.signal_workflow(cr, uid, [produce_id], 'button_confirm')
-            else:
-                res[procurement.id] = False
-                self.message_post(cr, uid, [procurement.id], body=_("No BoM exists for this product!"), context=context)
         return res
-
     
+#     def make_mo(self, cr, uid, ids, context={}):
+#         """ Make Manufacturing(production) order from procurement
+#         @return: New created Production Orders procurement wise
+#         """
+#         res = {}
+#         company = self.pool.get('res.users').browse(cr, uid, uid, context).company_id
+#         production_obj = self.pool.get('mrp.production')
+#         bom_obj = self.pool.get('mrp.bom')
+#         procurement_obj = self.pool.get('procurement.order')
+#         for procurement in procurement_obj.browse(cr, uid, ids, context=context):
+#             if self.check_bom_exists(cr, uid, [procurement.id], context=context):
+#                 if procurement.bom_id:
+#                     bom_id = procurement.bom_id.id
+#                     routing_id = procurement.bom_id.routing_id.id
+#                 else:
+#                     properties = [x.id for x in procurement.property_ids]
+#                     bom_id = bom_obj._bom_find(cr, uid, procurement.product_uom.id,
+#                         product_id=procurement.product_id.id, properties=properties)
+#                     bom = bom_obj.browse(cr, uid, bom_id, context=context)
+#                     routing_id = bom.routing_id.id
+# 
+#                 res_id = procurement.move_dest_id and procurement.move_dest_id.id or False
+#                 newdate = datetime.strptime(procurement.date_planned, '%Y-%m-%d %H:%M:%S') - relativedelta(days=procurement.product_id.produce_delay or 0.0)
+#                 newdate = newdate - relativedelta(days=company.manufacturing_lead)
+#                 #create the MO as SUPERUSER because the current user may not have the rights to do it (mto product launched by a sale for example)
+#                 sequence_number =  procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.name or ''
+#                 #if procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id :
+#                 produce_id = production_obj.create(cr, SUPERUSER_ID, {
+#                     'origin': procurement.origin,
+#                     'product_id': procurement.product_id.id,
+#                     'product_qty': procurement.product_qty,
+#                     'product_uom': procurement.product_uom.id,
+#                     'product_uos_qty': procurement.product_uos and procurement.product_uos_qty or False,
+#                     'product_uos': procurement.product_uos and procurement.product_uos.id or False,
+#                     'location_src_id': procurement.location_id.id,
+#                     'location_dest_id': procurement.location_id.id,
+#                     'bom_id': bom_id,
+#                     'routing_id': routing_id,
+#                     'date_planned': newdate.strftime('%Y-%m-%d %H:%M:%S'),
+#                     'move_prod_id': res_id,
+#                     'company_id': procurement.company_id.id,
+#                     'production_id':procurement.move_dest_id and procurement.move_dest_id.raw_material_production_id and procurement.move_dest_id.raw_material_production_id.id or False,
+#                     'name':sequence_number + (procurement.bom_seq and ('-' + str(procurement.bom_seq)) or '')
+#                 })
+#                 
+#                 res[procurement.id] = produce_id
+#                 self.write(cr, uid, [procurement.id], {'production_id': produce_id})
+# 
+#                 procurement.refresh()
+#                 self.production_order_create_note(cr, uid, procurement, context=context)
+#                 production_obj.action_compute(cr, uid, [produce_id], properties=[x.id for x in procurement.property_ids])
+#                 production_obj.signal_workflow(cr, uid, [produce_id], 'button_confirm')
+#             else:
+#                 res[procurement.id] = False
+#                 self.message_post(cr, uid, [procurement.id], body=_("No BoM exists for this product!"), context=context)
+#         return res
